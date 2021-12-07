8d709ad5b8b313cda446df241288a89fe777e1a1
chamodi <chamodhihansana9@gmail.com>
2021-12-07 10:41:03 +0530

    SignUp Inherited

---
+++ odoo-custom-addons/auth/controllers/controllers.py
@@ -0,0 +1,8 @@
+from odoo.addons.auth_signup.controllers.main import AuthSignupHome
+
+
+class AuthSignupStreet(AuthSignupHome):
+    def _signup_with_values(self, token, values):
+        context = self.get_auth_signup_qcontext()
+        values.update({'contact_no': context.get('contact_no'),'last_name': context.get('last_name')})
+        super(AuthSignupStreet, self)._signup_with_values(token, values)
---
+++ odoo-custom-addons/auth/models/user.py
@@ -0,0 +1,9 @@
+from odoo import api, fields, models
+
+class ResUsersExt(models.Model):
+    _inherit = 'res.users'
+
+    contact_no = fields.Char(string='Telephone Number', required=True)
+    last_name = fields.Char(string='Last Name', required=True)
+   
+   
