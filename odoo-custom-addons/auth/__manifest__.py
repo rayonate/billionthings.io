a1f58ba61d9b8412d3672f65e03bf9e088710579
chamodi <chamodhihansana9@gmail.com>
2022-01-13 14:42:09 +0530

    Signup validation not defines error fixed

--- odoo-custom-addons/auth/__manifest__.py
+++ odoo-custom-addons/auth/__manifest__.py
@@ -2,13 +2,12 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Auth',
-    'version': '1.0',
     'summary': 'Billiothings',
     'sequence': 10,
     'description': """Billiothings""",
-    'category': 'Productivity',
-    'website': 'https://www.odoo.com/app/billing',
-    'images': ['images/accounts.jpeg', 'images/bank_statement.jpeg', 'images/cash_register.jpeg', 'images/chart_of_accounts.jpeg', 'images/customer_invoice.jpeg', 'images/journal_entries.jpeg'],
+    'author': "Billionthings.io",
+    'category': 'Uncategorized',
+    'website': 'http://billiothings.io',
     'depends': ['base', 'website'],
     'data': [
         'security/ir.model.access.csv',
@@ -20,11 +19,9 @@
             'auth/static/js/auth.js'
         ]
     },
-    'demo': [],
     'installable': True,
     'application': True,
     'auto_install': False,
     'post_init_hook': '',
-    'assets': {},
     'license': 'LGPL-3',
 }
